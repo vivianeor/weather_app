1fbe1d477d2dcd68a20d324fb39bea4d6764ff7c
vivianeor <viviane.oliveira280@hotmail.com>
2023-03-16 20:28:55 -0300

    criação dos componentes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'features/home/presentation/ui/home_page.dart';
+import 'features/home/presentation/ui/page/home_page.dart';
 
 void main() {
   runApp(const WeatherApp());
