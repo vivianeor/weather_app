88f5f0f0602b1614de6511064172c28bf742fca4
vivianeor <viviane.oliveira280@hotmail.com>
2023-04-04 07:53:10 -0300

    alinhamento info

--- lib/features/home/presentation/ui/component/info_current_weather.component.dart
+++ lib/features/home/presentation/ui/component/info_current_weather.component.dart
@@ -12,32 +12,54 @@ class HourlyWeatherComponent extends StatefulWidget {
 class _HourlyWeatherComponentState extends State<HourlyWeatherComponent> {
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
-        Row(
+        Column(
           children: [
-            Icon(Icons.ac_unit),
-            TextLabel(label: '24º'),
-            Icon(Icons.ac_unit),
-            TextLabel(label: '11º'),
+            Row(
+              children: [
+                Icon(Icons.ac_unit),
+                TextLabel(label: '24º'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextLabel(label: 'humidade'),
+                TextLabelBold(label:'63%'),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextLabel(label:'uv index'),
+                TextLabelBold(label:'2'),
+              ],
+            ),
           ],
         ),
-        Row(
+        Column(
           children: [
-            TextLabel(label: 'humidade'),
-            TextLabelBold(label:'63%'),
-            TextLabel(label: 'vento'),
-            TextLabelBold(label:'5 m/s'),
+            Row(
+              children: [
+                Icon(Icons.ac_unit),
+                TextLabel(label: '11º'),
+              ],
+            ),
+            Row(
+              children: [
+                TextLabel(label: 'vento'),
+                TextLabelBold(label:'5 m/s'),
+              ],
+            ),
+            Row(
+              children: [
+                TextLabel(label:'pressão'),
+                TextLabelBold(label:'764'),
+              ],
+            )
           ],
         ),
-        Row(
-          children: [
-            TextLabel(label:'uv index'),
-            TextLabelBold(label:'2'),
-            TextLabel(label:'pressão'),
-            TextLabelBold(label:'764'),
-          ],
-        )
       ],
     );
   }
